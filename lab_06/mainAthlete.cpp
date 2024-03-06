c6ea9dc240b917dcf3f579f36b48a3f6dfa75628
wayneS0628 <wdsimmons2@icloud.com>
2024-03-06 18:34:41 -0500

    Fix speedGain implemntation and test/add finishing touches

--- lab_06/mainAthlete.cpp
+++ lab_06/mainAthlete.cpp
@@ -7,14 +7,19 @@ int main(){
     Athlete athlete1;
     Athlete Kyrie("Kyrie", 6.2, 15);
 	Athlete Luka("Luka", 6.7, 12);
+
 	
+	cout << endl;
 	cout << "Welcome to NBA Training Camp\n";
+	cout << endl;
 	cout << "Athlete " << Kyrie.getName() << ", Height: " << Kyrie.getHeight() << "feet, " << " Speed: " << Kyrie.getSpeed() << "mph\n";
 	cout << "Athlete " << Luka.getName() << ", Height: " << Luka.getHeight() << "feet, " << " Speed: " << Luka.getSpeed() << "mph\n";
+	cout << "Athlete " << athlete1.getName() << ", Height: " << athlete1.getHeight() << "feet, " << " Speed: " << athlete1.getSpeed() << "mph\n";
 	
 	
-	Kyrie.addAccleration(20);
-	Luka.addAccleration(20);
+	Kyrie.speedGain(20);
+	Luka.speedGain(20);
+	athlete1.speedGain(20);
 	
 	cout << "---------------------------\n";
 	cout << "After NBA Training Camp\n";
@@ -24,7 +29,11 @@ int main(){
 	
 	cout << "Athlete " << Kyrie.getName() << ", Height: " << Kyrie.getHeight() << "feet, " << " Speed: " << Kyrie.getSpeed() << "mph\n";
 	cout << "Athlete " << Luka.getName() << ", Height: " << Luka.getHeight() << "feet, " << " Speed: " << Luka.getSpeed() << "mph\n";
+	cout << "Athlete " << athlete1.getName() << ", Height: " << athlete1.getHeight() << "feet, " << " Speed: " << athlete1.getSpeed() << "mph\n";
+	cout << endl;
 	
 	cout<< "You guys have really improved.\n";
-	cout << "CONGRATULATIONS ON A GREAT TRAINING CAMP!!!";
+	cout << endl;
+	cout << "CONGRATULATIONS ON A GREAT TRAINING CAMP!!!\n";
+	cout << endl;
 }
