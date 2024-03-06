40bd7a2da3a6af57fa0dd08ee8238fed4498fafb
wayneS0628 <wdsimmons2@icloud.com>
2024-03-06 18:46:11 -0500

    create data members and member functions for class and initialize one default object

--- lab_05/lab05Athlete.cpp
+++ lab_05/lab05Athlete.cpp
@@ -4,16 +4,33 @@ using namespace std;
 
 class Athlete {
     private:
-
-
-
+		string name;
+		double height; // in feet
+		double speed; // in mph
     public:
+    	Athlete() {
+	        name = "anyAthlete";
+	        height = 6.2;
+	        speed = 15;
+}
+		Athlete(string theName, double theHeight, double theSpeed) : name(theName), height(theHeight), speed(theSpeed){}
+		string getName(){
+            return name;
+        }
+		double getHeight(){
+            return height;
+        }
+		double getSpeed(){
+            return speed;
+        }
 
-
-    
-
+		void speedGain(double percent){
+            speed = speed * (1 + (percent/100));
+        }
 };
 
 int main(){
+
+    Athlete athlete1;
 
 }
